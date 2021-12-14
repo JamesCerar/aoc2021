d6ad7618879bc5ad587198f0af991c419c735f78
James Cerar <james@uken.com>
2021-12-14 09:35:23 -0500

    write better comments for 14b

--- 14/14b.py
+++ 14/14b.py
@@ -33,10 +33,13 @@ for i in inputList:
 
 
 iters = 40
+
+#create dictionary with current pairs
 pairsDict = defaultdict(int)
 for j in range(0,len(pTemplate)-1):
     pairsDict[pTemplate[j:j+2]]+=1
 
+#for each pair - how many times did that pair appear? Split into two new pairs, and add to new iteration
 for i in range(0,iters):
     iterStart = time.time()
     print('iter '+str(i+1))
@@ -48,11 +51,13 @@ for i in range(0,iters):
     pairsDict = newPairsDict.copy()
     print(time.time() - iterStart)
 
+#count total number of letters in all pairs
 elementDict = defaultdict(int)
 for i in pairsDict:
     elementDict[i[0]] += pairsDict[i]
     elementDict[i[1]] += pairsDict[i]
 
+#add one for first and last letter in template (they'll always be there, and this method doesn't capture them), and then divide by 2
 firstLast = [pTemplate[0],pTemplate[-1]]
 for i in elementDict:
     if i in firstLast: elementDict[i] += 1
